49a41373f6145959a4540bc1c8fb229f794fcb7e
“Ina” <ina.kurets@yandex.by>
2022-06-17 16:54:54 +0300

    feature/JS-1: - add first files

--- script.js
+++ script.js
@@ -1,18 +1,33 @@
 let offset = 0;
-const sliderLine = document.querySelector('.slider-line');
+const sliderLine = document.querySelector('.slider__line');
+const prev = document.querySelector('.slider__prev');
+const next = document.querySelector('.slider__next');
 
-document.querySelector('.slider-next').addEventListener('click', function(){
-    offset = offset + 500;
-    if (offset > 2500) {
-        offset = 0;
+if (offset === 0) {
+    prev.disabled = true;
+}
+
+if (offset === 2500) {
+    next.disabled = true;
+}
+
+const scrollPrev = () => {
+    offset -= 500;
+    next.disabled = false;
+    if (offset === 0) {
+        prev.disabled = true;
     }
     sliderLine.style.left = -offset + 'px';
-});
-
-document.querySelector('.slider-prev').addEventListener('click', function () {
-    offset = offset - 500;
-    if (offset < 0) {
-        offset = 2500;
+};
+ 
+const scrollNext = () => {
+    offset += 500;
+    prev.disabled = false;
+    if (offset === 2500) {
+        next.disabled = true;
     }
     sliderLine.style.left = -offset + 'px';
-});
+}
+
+document.querySelector('.slider__prev').addEventListener('click', scrollPrev);
+document.querySelector('.slider__next').addEventListener('click', scrollNext);
